cf93598fd1cabb393386206ffd187e3b92213024
DY <67324580+pranayedam@users.noreply.github.com>
2025-06-29 23:56:39 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,7 +8,16 @@ output "eks_cluster_endpoint" {
   value       = module.eks.cluster_endpoint
 }
 
+output "eks_cluster_certificate_authority" {
+  description = "EKS Cluster certificate authority"
+  value       = module.eks.cluster_certificate_authority_data
+}
+
 output "eks_kubeconfig" {
-  description = "A kubectl configuration to access the EKS cluster"
-  value       = module.eks.kubeconfig
+  description = "Kubeconfig file for the EKS cluster"
+  value = templatefile("${path.module}/kubeconfig.tmpl", {
+    endpoint                = module.eks.cluster_endpoint
+    cluster_certificate     = module.eks.cluster_certificate_authority_data
+    cluster_name            = module.eks.cluster_name
+  })
 }
