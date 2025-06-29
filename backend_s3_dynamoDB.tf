9cba71fc904b8f96bd37b148d6212c255c376370
Nithish Kumar Edam <nithishkumar_edam@epam.com>
2025-06-29 19:58:26 +0530

    add s3 backend

---
+++ backend_s3_dynamoDB.tf
@@ -0,0 +1,42 @@
+# dynamodb-s3-backend.tf
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# S3 Bucket for Terraform State
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "my-terraform-state-bucket" # Make sure this is unique globally
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  tags = {
+    Name        = "Terraform-State-Bucket"
+    Environment = "Production"
+  }
+}
+
+# DynamoDB Table for State Lock
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform-Locks"
+    Environment = "Production"
+  }
+}
