52e2278c92c980d479432431606d4cdfb132cbc5
DY <67324580+pranayedam@users.noreply.github.com>
2025-06-29 22:59:17 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "my-terraform-state-bucket-nwewnre"
     key            = "eks/terraform.state"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
+    dynamodb_table = "terraform-locks-retywu"
     encrypt        = true
   }
 }
