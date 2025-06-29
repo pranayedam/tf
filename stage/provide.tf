6a85653fe43708bbf333b3787e22beaf8e6f37a2
DY <67324580+pranayedam@users.noreply.github.com>
2025-06-29 19:16:12 +0530

    Delete stage directory

--- stage/provide.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "drona36"
-    key     = "terraform.workspace"
-    region  = "ap-southeast-2" # Replace with your desired region
-    encrypt = true
-  }
-}
